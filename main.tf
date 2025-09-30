15b86b157160d1ace26011287d4a3b7cb93605be
Shinya Nakano <nakano.shinya@cloud-ace.jp>
2025-09-30 11:31:33 +0900

    test

--- main.tf
+++ main.tf
@@ -1,74 +1,78 @@
-# --------------------------------------
-# provider設定
-# --------------------------------------
-
-terraform {
-  backend "gcs" {
-    # Stateファイルを保存するGCSバケット名をここに固定する
-    bucket = "test-bucket-202509121525" 
-    prefix = "test/terraform.tfstate"
-  }
-  required_version = "= 1.8.4"
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "< 6.0.0"
-    }
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = "< 6.0.0"
-    }
-  }
-}
-
+# local 定義
 locals {
-  project = "ca-nakano-jb-build-test"
-  region  = "asia-northeast1"
-  bucket  = "test-bucket-202509121630"
+    github_repository           = "nakano-ca/cloud-build-cicd-test"
+    project_id                  = "ca-nakano-jb-build-test"
+    region                      = "asia-northeast1"
+    terraform_service_account   = "gha-demo-sac@ca-nakano-jb-build-test.iam.gserviceaccount.com"
+    
+    # api 有効化用
+    services = toset([                         # Workload Identity 連携用
+        "iam.googleapis.com",                  # IAM
+        "cloudresourcemanager.googleapis.com", # Resource Manager
+        "iamcredentials.googleapis.com",       # Service Account Credentials
+        "sts.googleapis.com"                   # Security Token Service API
+    ])
 }
-
-provider "google" {
-  project = local.project
-  region  = local.region
+  
+# provider 設定
+terraform {
+    required_providers {
+        google  = {
+            source  = "hashicorp/google"
+            version = ">= 4.0.0"
+        }
+    }
+    required_version = ">= 1.3.0"
+    backend "gcs" {
+        bucket = "myproject_terraform_tfstate"
+        prefix = "terraform/state"
+    }
 }
-
-# # 180秒間待機するだけのシンプルなリソース
-# resource "time_sleep" "wait_forA" {
-#   # `terraform apply` を実行した際に、この期間だけ待機します。
-#   create_duration = "180s"
-# }
-
-# 180秒間待機するだけのシンプルなリソース
-resource "time_sleep" "wait_forB" {
-  # `terraform apply` を実行した際に、この期間だけ待機します。
-  create_duration = "190s"
+  
+## API の有効化(Workload Identity 用)
+resource "google_project_service" "enable_api" {
+  for_each                   = local.services
+  project                    = local.project_id
+  service                    = each.value
+  disable_dependent_services = true
 }
-
-# # --------------------------------------
-# # Cloud Storage
-# # --------------------------------------
-
-# # バケット作成
-# resource "google_storage_bucket" "default" {
-#   name     = local.bucket
-#   location = local.region
-# }
-
-# # バックエンドサービスのステータス変更待機
-# resource "time_sleep" "test" {
-#   depends_on      = [google_storage_bucket.default]
-#   create_duration = "180s"
-# }
-
-# # サンプルコンテンツのアップロード
-# resource "google_storage_bucket_object" "image_txt" {
-#   name   = "image/test.txt"
-#   bucket = google_storage_bucket.default.name
-#   source = "./image/test.txt"
-# }
-
-# resource "google_storage_bucket_object" "image_png" {
-#   name   = "image/kitten.png"
-#   bucket = google_storage_bucket.default.name
-#   source = "./image/kitten.png"
-# }
+    
+# Workload Identity Pool 設定
+resource "google_iam_workload_identity_pool" "mypool" {
+    provider                  = google-beta
+    project                   = local.project_id
+    workload_identity_pool_id = "mypool"
+    display_name              = "mypool"
+    description               = "GitHub Actions で使用"
+}
+  
+# Workload Identity Provider 設定
+resource "google_iam_workload_identity_pool_provider" "myprovider" {
+    provider                           = google-beta
+    project                            = local.project_id
+    workload_identity_pool_id          = google_iam_workload_identity_pool.mypool.workload_identity_pool_id
+    workload_identity_pool_provider_id = "myprovider"
+    display_name                       = "myprovider"
+    description                        = "GitHub Actions で使用"
+    
+    attribute_mapping = {
+        "google.subject"       = "assertion.sub"
+        "attribute.repository" = "assertion.repository"
+    }
+    
+    oidc {
+        issuer_uri = "https://token.actions.githubusercontent.com"
+    }
+}
+  
+# GitHub Actions が借用するサービスアカウント
+data "google_service_account" "terraform_sa" {
+    account_id = local.terraform_service_account
+}
+  
+# サービスアカウントの IAM Policy 設定と GitHub リポジトリの指定
+resource "google_service_account_iam_member" "terraform_sa" {
+    service_account_id = data.google_service_account.terraform_sa.id
+    role               = "roles/iam.workloadIdentityUser"
+    member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.mypool.name}/attribute.repository/${local.github_repository}"
+}
