172c07b70de2535df8f07ce3e254706d96cdd8c5
Shinya Nakano <nakano.shinya@cloud-ace.jp>
2025-10-08 00:54:08 +0900

    test

--- main.tf
+++ main.tf
@@ -1,3 +1,29 @@
+resource "google_storage_bucket" "main" {
+  name          = "[PROJECT_ID]_static_website_bucket"
+  location      = "asia-northeast1"
+  storage_class = "STANDARD"
+  website {
+    main_page_suffix = "index.html"
+  }
+}
+
+resource "google_storage_bucket_object" "main" {
+  name         = "index.html"
+  content      = "<html><body>Hello World!</body></html>"
+  content_type = "text/html"
+  bucket       = google_storage_bucket.main.id
+}
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">=5.33.0"
+    }
+  }
+  required_version = "1.8.5"
+}
+
 # # local 定義
 # locals {
 #     github_repository           = "nakano-ca/cloud-build-cicd-test"
